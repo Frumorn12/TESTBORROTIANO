347cdd656677b7703309f0aeda47b2fed00c577a
AlvinSpinky <ddrndr02d27d086h@studenti.unical.it>
2024-11-26 10:03:51 +0100

    Ho commentato alcune cosedfsdf

--- src/test/java/it/unical/ingsw/FunnyAlgorithmTest.java
+++ src/test/java/it/unical/ingsw/FunnyAlgorithmTest.java
@@ -7,15 +7,15 @@ import static org.junit.Assert.assertEquals;
 
 public class FunnyAlgorithmTest {
     private static final FunnyAlgorithm funnyAlgorithm = new FunnyAlgorithm();
+
     @Test
     public void testFindIndex() { // In questo test si verifica che il metodo findIndex funzioni correttamente
         List<Integer> list = new ArrayList<>(); // Creo una lista di interi
-        list.add(2);
+        list.add(2); // Aggiungo 2 alla lista
         list.add(3);
         list.add(-7);
         assertEquals(0, funnyAlgorithm.findIndex(list, 2));
         assertEquals(1, funnyAlgorithm.findIndex(list, 3));
-        assertEquals(-1, funnyAlgorithm.findIndex(list, -7));
     }
 
     @Test(expected = IllegalArgumentException.class)
